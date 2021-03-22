f5c8f56d88960215bc88770f7ac007db94d72e20
Yavor.Panev <yavor.panev@arup.com>
2021-03-22 18:45:58 +0000

    added mesh config file

---
+++ src/b612/scrape_config.py
@@ -0,0 +1,9 @@
+per_mesh_run_order = {'1': ['cpu_step', 'cfl', 'max_div', 'min_div', 'vn', 'nrg_loss', 'hrr', 'lagr'],
+                     '3': ['cfl', 'cpu_step', 'max_div', 'min_div', 'vn', 'nrg_loss', 'hrr', 'lagr'],
+                     '4': ['max_div', 'cpu_step', 'cfl', 'min_div', 'vn', 'nrg_loss', 'hrr', 'lagr'],
+                     '5': ['min_div', 'cpu_step', 'cfl', 'max_div', 'vn', 'nrg_loss', 'hrr', 'lagr'],
+                     '6': ['vn', 'cpu_step', 'cfl', 'max_div', 'min_div', 'nrg_loss', 'hrr', 'lagr'],
+                     '7': ['lagr', 'hrr', 'nrg_loss', 'cfl', 'max_div', 'min_div', 'vn', 'cpu_step'],
+                     '8': ['hrr', 'nrg_loss', 'cfl', 'max_div', 'min_div', 'vn', 'cpu_step', 'lagr'],
+                     '9': ['nrg_loss', 'hrr', 'cfl', 'max_div', 'min_div', 'vn', 'cpu_step', 'lagr'],
+                     '10': ['nrg_loss', 'hrr', 'cfl', 'max_div', 'min_div', 'vn', 'cpu_step', 'lagr']}
