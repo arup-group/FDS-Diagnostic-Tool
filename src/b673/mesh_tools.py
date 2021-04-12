a5bba7d9f4949a3eaad1a5b95a826d19046df3f8
Yavor.Panev <yavor.panev@arup.com>
2021-04-12 22:02:45 +0100

    added files for b673

---
+++ src/b673/mesh_tools.py
@@ -0,0 +1,118 @@
+import re
+
+
+def get_mesh_info(fds_filepath, out_dict):
+    mesh_pattern = r'&MESH.+IJK\s*=\s*((?:(?:[-+]?[0-9]*\.?[0-9]+(?:[eE][-+]?[0-9]+)?)[\s,]*?){3}).+XB\s*=\s*((?:(?:[-+]?[0-9]*\.?[0-9]+(?:[eE][-+]?[0-9]+)?)[\s,]*?){6})'
+    pattern_split = r'[\s,]+'
+    mesh_dict = {}
+    count = 0
+
+    with open(fds_filepath, "r") as file:
+        for line in file:
+            result = re.search(mesh_pattern, line)
+
+            if result is not None:
+                count += 1
+
+                mesh_size = [int(i) for i in re.split(pattern_split, result.group(1))]
+                mesh_dim = [float(i) for i in re.split(pattern_split, result.group(2))]
+
+                # TODO write it as dictionary of dictionaries
+                mesh_dict['mesh_{}'.format(count)] = mesh_size + mesh_dim
+
+    out_dict['mesh_info'] = mesh_dict
+    return out_dict
+
+
+def get_tot_els(out_dict):
+    '''Calculates total elements from scraped mesh info'''
+
+    from functools import reduce
+
+    out_dict['tot_el'] = 0
+
+    for key in out_dict['mesh_info'].keys():
+        mesh_el_sum = reduce((lambda x, y: x * y), out_dict['mesh_info'][key][0:3])
+        out_dict['mesh_info'][key] += [mesh_el_sum]
+        out_dict['tot_el'] += mesh_el_sum
+
+    return out_dict
+
+
+def get_tot_volume(out_dict):
+    '''Calculates total volume of model in m3'''
+
+    out_dict['tot_volume'] = 0
+
+    for mesh in out_dict['mesh_info']:
+        mesh_list = out_dict['mesh_info'][mesh]
+        out_dict['tot_volume'] += (mesh_list[4] - mesh_list[3]) * (mesh_list[6] - mesh_list[5]) * (
+                mesh_list[8] - mesh_list[7])
+
+    return out_dict
+
+
+def get_model_range(out_dict):
+    '''Calculates total volume of model in m3'''
+
+    out_dict['range'] = {}
+    xmin = 1000000
+    xmax = -1000000
+    ymin = 1000000
+    ymax = -1000000
+    zmin = 1000000
+    zmax = -1000000
+
+    for mesh in out_dict['mesh_info']:
+
+        mesh_list = out_dict['mesh_info'][mesh]
+
+        if mesh_list[3] < xmin:
+            xmin = mesh_list[3]
+
+        if mesh_list[4] > xmax:
+            xmax = mesh_list[4]
+
+        if mesh_list[5] < ymin:
+            ymin = mesh_list[5]
+
+        if mesh_list[6] > ymax:
+            ymax = mesh_list[6]
+
+        if mesh_list[7] < zmin:
+            zmin = mesh_list[7]
+
+        if mesh_list[8] > zmax:
+            zmax = mesh_list[8]
+
+        out_dict['range']['dx'] = xmax - xmin
+        out_dict['range']['dy'] = ymax - ymin
+        out_dict['range']['dz'] = zmax - zmin
+
+    return out_dict
+
+
+def get_grid_size(out_dict):
+    '''Calculates the grid size for each mesh'''
+
+    mesh_sizes = {}
+
+    for key in out_dict['mesh_info'].keys():
+        mesh_sizes[key] = round((out_dict['mesh_info'][key][4] -
+                                 out_dict['mesh_info'][key][3]) / out_dict['mesh_info'][key][0], 2)
+
+    out_dict['grid_size'] = mesh_sizes
+
+    return out_dict
+
+
+def mesh_als(fds_path):
+    mesh_data = {}
+
+    mesh_data = get_mesh_info(fds_path, mesh_data)
+    mesh_data = get_tot_els(mesh_data)
+    mesh_data = get_grid_size(mesh_data)
+    mesh_data = get_tot_volume(mesh_data)
+    mesh_data = get_model_range(mesh_data)
+
+    return mesh_data
---
+++ src/b673/scraping_fxn.py
@@ -0,0 +1,414 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 25 17:01:01 2019
+
+@author: javor
+"""
+
+import re
+import datetime
+from b673.analysis_fxn import calc_loc
+
+
+def ver(input_str, out_dict):
+    pattern = r'Version[\s:A-Za-z]+(\d+\.\d+\.\d+)'
+    result = re.search(pattern, input_str)
+
+    if result is not None:
+        out_dict['ver'] = result.group(1)
+
+    return out_dict
+
+def stop_cond(input_str, out_dict):
+
+    if 'completed successfully' in input_str:
+        out_dict['stop'] = 'completed'
+    elif 'stopped by user' in input_str:
+        out_dict['stop'] = 'user'
+    return out_dict
+
+
+    return out_dict
+
+def sim_end(input_str, out_dict):
+    pattern = r'[Ee]nd\s+[Tt]ime\s+\(s\)\s+(([-+]?[0-9]*\.?[0-9]+)([eE]([-+])?([0-9]+))?)'
+    result = re.search(pattern, input_str)
+
+    if result is not None:
+        out_dict['sim_end'] = float(result.group(1))
+
+    return out_dict
+
+
+def date_start(input_str, out_dict):
+    pattern = r'[A-Za-z]{3,}\s+\d+,\s+\d+\s+\d{2}:\d{2}:\d{2}'
+    result = re.search(pattern, input_str)
+
+    if result is not None:
+        date_str = result.group(0)
+        out_dict['date_start'] = datetime.datetime.strptime(date_str, "%B %d, %Y %H:%M:%S")
+
+    return out_dict
+
+
+def cores_n(input_str, out_dict):
+    pattern = r'MPI\s+[Pp]rocesses:\s+([0-9]+)'
+    result = re.search(pattern, input_str)
+
+    if result is not None:
+        out_dict['cores_n'] = int(result.group(1))
+
+    return out_dict
+
+
+def mesh_info(fds_filepath, out_dict):
+    mesh_pattern = r'&MESH.+IJK\s*=\s*((?:(?:[-+]?[0-9]*\.?[0-9]+(?:[eE][-+]?[0-9]+)?)[\s,]*?){3}).+XB\s*=\s*((?:(?:[-+]?[0-9]*\.?[0-9]+(?:[eE][-+]?[0-9]+)?)[\s,]*?){6})'
+    pattern_split = r'[\s,]+'
+    mesh_dict = {}
+    count = 0
+
+    with open(fds_filepath, "r") as file:
+        for line in file:
+            result = re.search(mesh_pattern, line)
+
+            if result is not None:
+                count += 1
+
+                mesh_size = [int(i) for i in re.split(pattern_split, result.group(1))]
+                mesh_dim = [float(i) for i in re.split(pattern_split, result.group(2))]
+
+                # TODO write it as dictionary of dictionaries
+                mesh_dict['mesh_{}'.format(count)] = mesh_size + mesh_dim
+
+    out_dict['mesh_info'] = mesh_dict
+    return out_dict
+
+
+def tot_elp_time(input_str, out_dict):
+    pattern = r'[Ss]tepping.+:\s+([-+]?[0-9]*\.?[0-9]+(?:[eE][-+]?[0-9]+)?)'
+    result = re.search(pattern, input_str)
+
+    if result is not None:
+        out_dict['tot_elp_time'] = round(float(result.group(1)))
+
+    return out_dict
+
+
+def itr_date(input_str, out_dict, **kwargs):
+    pattern = r'[Tt]ime\s+[Ss]tep\s+(\d+)\s+([A-Za-z]{3,}\s+\d+,\s+\d+\s+\d{2}:\d{2}:\d{2})'
+    result = re.search(pattern, input_str)
+
+    if result is not None:
+        out_dict['cycles'] = int(result.group(1))
+        date_str = result.group(2)
+        out_dict['log_time'] = datetime.datetime.strptime(date_str, "%B %d, %Y %H:%M:%S")
+
+    return out_dict
+
+
+def log_time(input_str, out_dict, **kwargs):
+    pattern = r'[Tt]ime\s+[Ss]tep\s+(\d+)\s+([A-Za-z]{3,}\s+\d+,\s+\d+\s+\d{2}:\d{2}:\d{2})'
+    result = re.search(pattern, input_str)
+
+    if result is not None:
+        date_str = result.group(2)
+        out_dict['log_time'] = datetime.datetime.strptime(date_str, "%B %d, %Y %H:%M:%S")
+
+    return out_dict
+
+
+def cycles(input_str, out_dict, **kwargs):
+    pattern = r'[Tt]ime\s+[Ss]tep\s+(\d+)\s+([A-Za-z]{3,}\s+\d+,\s+\d+\s+\d{2}:\d{2}:\d{2})'
+    result = re.search(pattern, input_str)
+
+    if result is not None:
+        out_dict['cycles'] = int(result.group(1))
+
+    return out_dict
+
+
+def time_step(input_str, out_dict, **kwargs):
+    pattern = r'[Tt]ime\s[Ss]tep:\s+((?:[-+]?[0-9]*\.?[0-9]+)(?:[eE][-+]?[0-9]+)?)'
+    result = re.search(pattern, input_str)
+
+    if result is not None:
+        out_dict['ts'] = float(result.group(1))
+
+    return out_dict
+
+
+def sim_time(input_str, out_dict, **kwargs):
+    pattern = r'Total\stime:\s+((?:[-+]?[0-9]*\.?[0-9]+)(?:[eE][-+]?[0-9]+)?)'
+    result = re.search(pattern, input_str)
+
+    if result is not None:
+        out_dict['sim_time'] = float(result.group(1))
+
+    return out_dict
+
+
+def press_itr(input_str, out_dict, **kwargs):
+    pattern = r'Iterations:\s+((?:[-+]?[0-9]*\.?[0-9]+)(?:[eE][-+]?[0-9]+)?)'
+    result = re.search(pattern, input_str)
+
+    if result is not None:
+        out_dict['press_itr'] = int(result.group(1))
+
+    return out_dict
+
+
+def vel_err(input_str, out_dict, mesh_info=None, **kwargs):
+    pattern = r'Velocity\sError:\s+((?:[-+]?[0-9]*\.?[0-9]+)(?:[eE][-+]?[0-9]+)?)[a-zA-Z\s]+\s+(\d+)[a-z\s]+\((?:\s+)?(\d+,?\s+\d+,?\s+\d+)'
+    result = re.search(pattern, input_str)
+
+    pattern_split = r'[\s,]+'
+
+    if result is not None:
+        out_dict['vel_err'] = float(result.group(1))
+        out_dict['vel_err_m'] = int(result.group(2))
+
+        m_error_loc = [int(i) for i in re.split(pattern_split, result.group(3))]
+
+        if mesh_info is not None:
+            out_dict['vel_err_loc'] = calc_loc(m_error_loc, mesh_info, out_dict['vel_err_m'])
+        else:
+            out_dict['vel_err_loc'] = m_error_loc
+
+    return out_dict
+
+def press_err(input_str, out_dict, mesh_info=None, **kwargs):
+    pattern = r'Pressure\sError:\s+((?:[-+]?[0-9]*\.?[0-9]+)(?:[eE][-+]?[0-9]+)?)[a-zA-Z\s]+\s+(\d+)[a-z\s]+\((?:\s+)?(\d+,?\s+\d+,?\s+\d+)'
+    result = re.search(pattern, input_str)
+
+    pattern_split = r'[\s,]+'
+
+    if result is not None:
+        out_dict['press_err'] = float(result.group(1))
+        out_dict['press_err_m'] = int(result.group(2))
+
+        m_error_loc = [int(i) for i in re.split(pattern_split, result.group(3))]
+
+        if mesh_info is not None:
+            out_dict['press_err_loc'] = calc_loc(m_error_loc, mesh_info, out_dict['vel_err_m'])
+        else:
+            out_dict['press_err_loc'] = m_error_loc
+
+    return out_dict
+
+
+def mesh_n(input_str, n, mesh_line):
+    pattern = r'Mesh\s+(\d+)$'
+    result = re.search(pattern, input_str)
+
+    if result is not None:
+        n = int(result.group(1))
+        mesh_line = 0
+
+    return n, mesh_line
+
+
+def cfl_n(input_str, out_dict, outcome, n_mesh, mesh_info=None, **kwargs):
+    pattern = r'CFL number:\s+((?:[-+]?[0-9]*\.?[0-9]+)(?:[eE][-+]?[0-9]+)?)[a-z\s]+\((?:\s+)?(\d+,?(?:\s+)?\d+,?(?:\s+)?\d+)'
+    pattern_split = r'[\s,]+'
+    result = re.search(pattern, input_str)
+
+    if result is not None:
+
+        outcome = True
+        cfl_loc = [int(i) for i in re.split(pattern_split, result.group(2))]
+        out_dict['cfl']['m{}'.format(n_mesh)] = float(result.group(1))
+
+        if mesh_info is not None:
+            out_dict['cfl']['m{}_loc'.format(n_mesh)] = calc_loc(cfl_loc, mesh_info, n_mesh)
+        else:
+            out_dict['cfl']['m{}_loc'.format(n_mesh)] = cfl_loc
+
+    else:
+        outcome = False
+
+    return out_dict, outcome
+
+
+def max_div(input_str, out_dict, outcome, n_mesh, mesh_info=None, **kwargs):
+    pattern = r'Max divergence:\s+((?:[-+]?[0-9]*\.?[0-9]+)(?:[eE][-+]?[0-9]+)?)[a-z\s]+\((?:\s+)?(\d+,?(?:\s+)?\d+,?(?:\s+)?\d+)'
+    pattern_split = r'[\s,]+'
+    result = re.search(pattern, input_str)
+
+    if result is not None:
+
+        outcome = True
+        max_div_loc = [int(i) for i in re.split(pattern_split, result.group(2))]
+        out_dict['max_div']['m{}'.format(n_mesh)] = float(result.group(1))
+
+        if mesh_info is not None:
+            out_dict['max_div']['m{}_loc'.format(n_mesh)] = calc_loc(max_div_loc, mesh_info, n_mesh)
+        else:
+            out_dict['max_div']['m{}_loc'.format(n_mesh)] = max_div_loc
+
+    else:
+        outcome = False
+
+    return out_dict, outcome
+
+
+def min_div(input_str, out_dict, outcome, n_mesh, mesh_info=None, **kwargs):
+    pattern = r'Min divergence:\s+((?:[-+]?[0-9]*\.?[0-9]+)(?:[eE][-+]?[0-9]+)?)[a-z\s]+\((?:\s+)?(\d+,?(?:\s+)?\d+,?(?:\s+)?\d+)'
+    pattern_split = r'[\s,]+'
+    result = re.search(pattern, input_str)
+
+    if result is not None:
+
+        outcome = True
+        min_div_loc = [int(i) for i in re.split(pattern_split, result.group(2))]
+        out_dict['min_div']['m{}'.format(n_mesh)] = float(result.group(1))
+
+        if mesh_info is not None:
+            out_dict['min_div']['m{}_loc'.format(n_mesh)] = calc_loc(min_div_loc, mesh_info, n_mesh)
+        else:
+            out_dict['min_div']['m{}_loc'.format(n_mesh)] = min_div_loc
+
+    else:
+        outcome = False
+
+    return out_dict, outcome
+
+
+def loss_bound(input_str, out_dict, outcome, n_mesh, **kwargs):
+    pattern = r'[Bb]oundaries:\s+((?:[-+]?[0-9]*\.?[0-9]+)(?:[eE][-+]?[0-9]+)?)'
+    result = re.search(pattern, input_str)
+
+    if result is not None:
+        outcome = True
+        out_dict['nrg_loss']['m{}'.format(n_mesh)] = float(result.group(1))
+        out_dict['nrg_loss']['ctrl'] = 1
+
+    else:
+        out_dict['nrg_loss']['ctrl'] = 0
+        outcome = False
+
+    return out_dict, outcome
+
+
+def hrr(input_str, out_dict, outcome, n_mesh, **kwargs):
+    pattern = r'[Rr]ate:\s+((?:[-+]?[0-9]*\.?[0-9]+)(?:[eE][-+]?[0-9]+)?)'
+    result = re.search(pattern, input_str)
+
+    if result is not None:
+        outcome = True
+        out_dict['hrr']['m{}'.format(n_mesh)] = float(result.group(1))
+        out_dict['hrr']['ctrl'] = 1
+
+    else:
+        out_dict['hrr']['ctrl'] = 0
+        outcome = False
+
+    return out_dict, outcome
+
+
+def lagrange(input_str, out_dict, outcome, n_mesh, **kwargs):
+    pattern = r'[Pp]articles:\s+((?:[-+]?[0-9]*\.?[0-9]+)(?:[eE][-+]?[0-9]+)?)'
+    result = re.search(pattern, input_str)
+
+    if result is not None:
+        outcome = True
+        out_dict['lagr']['m{}'.format(n_mesh)] = float(result.group(1))
+        out_dict['lagr']['ctrl'] = 1
+
+    else:
+        out_dict['lagr']['ctrl'] = 0
+        outcome = False
+
+    return out_dict, outcome
+
+
+def vn_n(input_str, out_dict, outcome, n_mesh, mesh_info=None, **kwargs):
+    pattern = r'VN\s+number:\s+((?:[-+]?[0-9]*\.?[0-9]+)(?:[eE][-+]?[0-9]+)?)[a-z\s]+\((?:\s+)?(\d+,?(?:\s+)?\d+,?(?:\s+)?\d+)'
+    pattern_split = r'[\s,]+'
+    result = re.search(pattern, input_str)
+
+    if result is not None:
+
+        outcome = True
+        vn_loc = [int(i) for i in re.split(pattern_split, result.group(2))]
+        out_dict['vn']['m{}'.format(n_mesh)] = float(result.group(1))
+
+        if mesh_info is not None:
+            out_dict['vn']['m{}_loc'.format(n_mesh)] = calc_loc(vn_loc, mesh_info, n_mesh)
+        else:
+            out_dict['vn']['m{}_loc'.format(n_mesh)] = vn_loc
+
+    else:
+        outcome = False
+
+    return out_dict, outcome
+
+
+def cpu_step(input_str, out_dict, outcome, n_mesh, **kwargs):
+    pattern = r'CPU\/step:\s+((?:[-+]?[0-9]*\.?[0-9]+)(?:[eE][-+]?[0-9]+)?)[a-zA-z,\s]+CPU:\s+([-+]?[0-9]*\.?[0-9]+)\s+(\w{1,3})'
+    result = re.search(pattern, input_str)
+
+    if result is not None:
+
+        outcome = True
+        out_dict['cpu_step']['m{}'.format(n_mesh)] = float(result.group(1))
+        #        out_dict['cpu_tot_m{}'.format(n_mesh)] = float(result.group(2))
+
+        if result.group(3) == 's':
+            out_dict['cpu_tot']['m{}'.format(n_mesh)] = float(result.group(2))
+
+        elif result.group(3) == 'min':
+            out_dict['cpu_tot']['m{}'.format(n_mesh)] = float(result.group(2)) * 60
+
+        elif result.group(3) == 'hr':
+            out_dict['cpu_tot']['m{}'.format(n_mesh)] = float(result.group(2)) * 3600
+
+    else:
+        outcome = False
+
+    return out_dict, outcome
+
+
+basic_param = {
+    'ver': ver,
+    'stop_cond' : stop_cond,
+    'sim_end': sim_end,
+    'date_start': date_start,
+    'cores_n': cores_n,
+    'tot_elp_time': tot_elp_time,
+    'itr_date': itr_date,
+    'ts': time_step,
+    'sim_time': sim_time,
+    'press_itr': press_itr,
+    'vel_err': vel_err,
+    'cfl': cfl_n,
+    'max_div': max_div,
+    'min_div': min_div,
+    'nrg_loss': loss_bound,
+    'hrr': hrr,
+    'vn': vn_n,
+    'cpu_step': cpu_step,
+    'log_time': log_time,
+    'cycles': cycles,
+    'lagr': lagrange,
+    'press_err': press_err}
+
+per_mesh_run_order = {'1': ['cfl', 'max_div', 'min_div', 'vn', 'nrg_loss', 'hrr', 'lagr'],
+                     '2': ['max_div', 'min_div', 'vn', 'nrg_loss', 'hrr', 'lagr', 'cfl'],
+                     '3': ['min_div', 'cfl', 'max_div', 'vn', 'nrg_loss', 'hrr', 'lagr'],
+                     '4': ['vn', 'cfl', 'max_div', 'min_div', 'nrg_loss', 'hrr', 'lagr'],
+                     '5': ['lagr', 'hrr', 'nrg_loss', 'cfl', 'max_div', 'min_div', 'vn'],
+                     '6': ['hrr', 'nrg_loss', 'cfl', 'max_div', 'min_div', 'vn', 'lagr'],
+                     '7': ['nrg_loss', 'hrr', 'cfl', 'max_div', 'min_div', 'vn', 'lagr']}
+
+
+def scrape(name, input_str, out_dict, **kwargs):
+    out_dict = basic_param[name](input_str, out_dict, **kwargs)
+
+
+def scrape_succs(name, input_str, out_dict, outcome, **kwargs):
+    out_dict, outcome = basic_param[name](input_str, out_dict, outcome, **kwargs)
+    return outcome
+
+
+def scrape_m(name, input_str, out_dict, n_mesh):
+    out_dict = basic_param[name](input_str, out_dict, n_mesh)
