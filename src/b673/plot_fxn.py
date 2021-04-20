e615fafab7b9baf902ba7f95c4d134746e5baab0
Yavor.Panev <yavor.panev@arup.com>
2021-04-20 18:35:37 +0100

    Refractor code so that data loading is done outside plotting file

--- src/b673/plot_fxn.py
+++ src/b673/plot_fxn.py
@@ -102,7 +102,7 @@ def format_ax(data, ax, total_time_span):
             break
 
 
-def mesh_stats_plot(data_loc, data_type, subplot=False, ax=None):
+def mesh_stats_plot(data, data_type, subplot=False, ax=None):
     # Function starts here
     plot_type = {'min_div': ['Min divergence', 'Simulation time (s)'],
                  'max_div': ['Max divergence', 'Simulation time (s)'],
@@ -110,7 +110,6 @@ def mesh_stats_plot(data_loc, data_type, subplot=False, ax=None):
                  'vn': ['VN number', 'Simulation time (s)'],
                  'cpu_step': ['Max time per timestep (s)', 'Simulation time (s)']}
 
-    data = pd.read_csv(os.path.join(data_loc, data_type + '.csv'))
     r = re.compile('^m\d+$')
     sorted_columns = list(filter(r.match, list(data.columns)))
 
@@ -210,6 +209,104 @@ def mesh_stats_plot(data_loc, data_type, subplot=False, ax=None):
         ax = plt.ylim([0, data['est'].mean() + 5 * data['est'].std()])
 
 
+def cycle_stats_pm_plot(data, data_type, subplot=False, ax=None):
+    # Function starts here
+    plot_type = {'vel_err': ['Velocity error (m/s)', 'Simulation time (s)'],
+                 'press_err': ['Pressure error', 'Simulation time (s)']}
+
+
+    data['est'] = data[data_type]
+    data['mesh'] = data[f'{data_type}_m']
+
+    plot_data = {}
+
+    # Assign colors
+    colors = sns.color_palette()
+    colors_dict = {}
+    k = 0
+    for i, entry in data['mesh'].value_counts().iteritems():
+        if k > 9:
+            colors_dict[i] = colors[9]
+        else:
+            colors_dict[i] = colors[k]
+        k = k + 1
+
+    for k, mesh in enumerate(data['mesh'].unique()):
+
+        list_dicts = []
+        plot_dict = {}
+
+        index_sorted = data[data['mesh'] == mesh].index.values
+        diff_index = np.diff(index_sorted)
+        res = np.where(diff_index > 1)
+
+        if len(index_sorted) > 1:
+            if res[0].size == 0:
+                plot_dict['start'] = index_sorted[0]
+                plot_dict['end'] = index_sorted[-1]
+                list_dicts.append(plot_dict)
+
+            else:
+                for k, index in enumerate(res[0]):
+                    if k == 0:
+                        plot_dict['start'] = index_sorted[0]
+                        plot_dict['end'] = index_sorted[index]
+
+                    else:
+                        plot_dict['start'] = index_sorted[res[0][k - 1] + 1]
+                        plot_dict['end'] = index_sorted[index]
+
+                    list_dicts.append(plot_dict)
+                    plot_dict = {}
+
+                # Final curve segment
+                plot_dict['start'] = index_sorted[res[0][-1] + 1]
+                plot_dict['end'] = index_sorted[-1]
+                list_dicts.append(plot_dict)
+
+        else:
+            plot_dict['start'] = index_sorted[0]
+            plot_dict['end'] = index_sorted[0]
+            list_dicts.append(plot_dict)
+
+        plot_data[mesh] = list_dicts
+
+    if subplot == False:
+        sns.set()
+        fig, ax = plt.subplots(figsize=(15, 6))
+
+    for mesh in plot_data:
+        for k in plot_data[mesh]:
+            x = data.loc[k['start']:k['end'], 'sim_time']
+            y = data.loc[k['start']:k['end'], 'est']
+
+            if k['start'] != 0:
+                x = x.append(
+                    pd.Series(0.5 * (data.loc[k['start'], 'sim_time'] + data.loc[k['start'] - 1, 'sim_time'])))
+
+            if k['end'] != data.index.max():
+                x = x.append(0.5 * (pd.Series(data.loc[k['end'], 'sim_time'] + data.loc[k['end'] + 1, 'sim_time'])))
+
+            if k['start'] != 0:
+                y = y.append(0.5 * (pd.Series(data.loc[k['start'], 'est'] + data.loc[k['start'] - 1, 'est'])))
+
+            if k['end'] != data.index.max():
+                y = y.append(0.5 * (pd.Series(data.loc[k['end'], 'est'] + data.loc[k['end'] + 1, 'est'])))
+
+            new_x, new_y = zip(*sorted(zip(x, y)))
+            ax = plt.plot(new_x, new_y, '-', color=colors_dict[mesh], label=mesh)
+
+    # Legend setup
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    ax = plt.legend(by_label.values(), by_label.keys(), ncol=4, loc=4)
+    ax = plt.xlabel(plot_type[data_type][1])
+    ax = plt.ylabel(plot_type[data_type][0])
+
+    if subplot == False:
+        ax.plot()
+
+
 def cycle_stats_plot(data, data_type, subplot=False, ax=None):
     # Function starts here
     plot_type = {'ts': ['Time step (s)', 'Simulation time (s)'],
@@ -264,27 +361,36 @@ def lagrange_plot(data_loc):
     return data
 
 
-def hrr_plot(data_loc):
-    data = pd.read_csv(os.path.join(data_loc, 'hrr.csv'))
+def hrr_plot(data_loc, subplot=False, ax=None):
+    data_hrr = pd.read_csv(os.path.join(data_loc, 'hrr.csv'))
+    nrg_loss = pd.read_csv(os.path.join(data_loc, 'nrg_loss.csv'))
 
     r = re.compile('^m\d+$')
 
-    sorted_columns = list(filter(r.match, list(data.columns)))
-    data['est'] = data[sorted_columns].sum(axis=1)
+    sorted_columns = list(filter(r.match, list(data_hrr.columns)))
+    data_hrr['est'] = data_hrr[sorted_columns].sum(axis=1)
+    sorted_columns = list(filter(r.match, list(nrg_loss.columns)))
+    nrg_loss['est'] = nrg_loss[sorted_columns].sum(axis=1)
 
-    fig, ax = plt.subplots(figsize=(15, 6))
-    sns.set()
-    colors = sns.color_palette()
-    ax = plt.plot(data['sim_time'], data['est'], '-', color=colors[0], label='HRR output')
+    if subplot is False:
+        fig, ax = plt.subplots(figsize=(15, 6))
+        sns.set()
+
+    ax = plt.plot(data_hrr['sim_time'], data_hrr['est'], '-', color='#4C72B0', label='HRR output')
+    ax = plt.plot(data_hrr['sim_time'], nrg_loss['est'].abs(), '-', color='#DD8452', label='Rad. loss')
+
     try:
         fire_curve = pd.read_csv(os.path.join(data_loc, 'fire_curve.csv'))
         ax = plt.plot(fire_curve['t'], fire_curve['tot'], '--', color='red', label='HRR definition')
     except:
         pass
 
-    ax = plt.ylabel('Total HRR (KW)')
+    ax = plt.ylabel('Total energy (KW)')
     ax = plt.xlabel('Simulation time (s)')
     ax = plt.legend()
+
+    if subplot is False:
+        plt.show()
 
 
 def derived_cpu_step_plot(data, subplot=False, ax=None):
@@ -315,9 +421,9 @@ def log_interval_plot(data, subplot=False, ax=None):
         fig, ax = plt.subplots()
 
 
-    p1, = ax.plot(data['log_time'], data['time_diff']/60, '-', color = '#4C72B0', label='log interv.')
+    p1, = ax.plot(data['log_time'], data['time_diff']/60, '-', color='#4C72B0', label='log interv.')
     twin1 = ax.twinx()
-    p2, = twin1.plot(data['log_time'], data['sim_time_diff'], '-', color = '#DD8452', label= 'sim time incr.')
+    p2, = twin1.plot(data['log_time'], data['sim_time_diff'], '-', color='#DD8452', label='sim time incr.')
 
     # Format axis
     format_ax(data, ax, total_time_span)
--- src/b673/plot_setup.py
+++ src/b673/plot_setup.py
@@ -5,37 +5,44 @@ import seaborn as sns
 import datetime
 import os
 import json
-from matplotlib import gridspec
+
+#TODO Update graph requirements as per key file
+#TODO write main graph function
+#TODO loc plots
+#TODO main function controllin plots
 
 def mesh_plots(output_loc):
-
-    data_cycles = pd.read_csv(os.path.join(output_loc, 'data', 'cycle_info.csv'), parse_dates=['log_time'])
-    data_loc = os.path.join(output_loc, 'data')
 
     sns.set()
     fig = plt.figure(figsize=(15,9))
 
     ax1 = plt.subplot(321)
-    plf.cycle_stats_plot(data_cycles, data_type='ts', subplot=True, ax=ax1)
+    data = pd.read_csv(os.path.join(output_loc, 'data', 'cycle_info.csv'))
+    plf.cycle_stats_plot(data, data_type='ts', subplot=True, ax=ax1)
 
-    ax2 = plt.subplot(323)
-    plf.mesh_stats_plot(data_loc, data_type='cfl', subplot=True, ax=ax2)
+    ax2 = plt.subplot(323, sharex=ax1)
+    data = pd.read_csv(os.path.join(output_loc, 'data', 'cfl.csv'))
+    plf.mesh_stats_plot(data, data_type='cfl', subplot=True, ax=ax2)
 
-    ax3 = plt.subplot(322)
-    plf.mesh_stats_plot(data_loc, data_type='min_div', subplot=True, ax=ax3)
+    ax3 = plt.subplot(322, sharex=ax1)
+    data = pd.read_csv(os.path.join(output_loc, 'data', 'min_div.csv'))
+    plf.mesh_stats_plot(data, data_type='min_div', subplot=True, ax=ax3)
     #
-    ax4 = plt.subplot(324)
-    plf.mesh_stats_plot(data_loc, data_type = 'max_div', subplot = True, ax = ax4)
+    ax4 = plt.subplot(324, sharex=ax1)
+    data = pd.read_csv(os.path.join(output_loc, 'data', 'max_div.csv'))
+    plf.mesh_stats_plot(data, data_type = 'max_div', subplot = True, ax=ax4)
 
-    ax5 = plt.subplot(325)
-    plf.derived_cpu_step_plot(data_cycles, subplot=True, ax=ax5)
+    ax5 = plt.subplot(325, sharex=ax1)
+    data = pd.read_csv(os.path.join(output_loc, 'data', 'cycle_info.csv'), parse_dates=['log_time'])
+    plf.derived_cpu_step_plot(data, subplot=True, ax=ax5)
 
     ax6 = plt.subplot(326, sharex=ax1)
-    plf.mesh_stats_plot(data_loc, data_type='vn', subplot=True, ax=ax6)
+    data = pd.read_csv(os.path.join(output_loc, 'data', 'vn.csv'))
+    plf.mesh_stats_plot(data, data_type='vn', subplot=True, ax=ax6)
 
     fig.suptitle(f'Last Updated: {datetime.datetime.now().strftime("%d-%b-%Y %H:%M")}', fontsize=12, va='top')
     plt.tight_layout()
-    plt.savefig(os.path.join(output_loc, 'mesh_plots.png'), bbox_inches = "tight")
+    plt.savefig(os.path.join(output_loc, 'mesh_plots.png'), bbox_inches="tight")
     plt.show()
 
 
@@ -44,7 +51,7 @@ def sim_progress(output_loc, analytics):
     with open(os.path.join(output_loc, 'data', 'sim_info.json')) as f:
         sim_info = json.load(f)
     data = pd.read_csv(os.path.join(output_loc, 'data', 'cycle_info.csv'), parse_dates=['log_time'])
-    data = data.iloc[0:200]
+    data = data.iloc[0:1000]
 
     sns.set()
     fig = plt.figure(figsize=(15,9))
@@ -57,7 +64,7 @@ def sim_progress(output_loc, analytics):
     plf.timeprogress_bar_plot(data, sim_info, t_predict=analytics["pred"], subplot=True, ax=ax1)
     ax2 = fig.add_subplot(spec[2, 0])
     plf.log_interval_plot(data, subplot=True, ax=ax2)
-    ax3 = fig.add_subplot(spec[2, 2])
+    ax3 = fig.add_subplot(spec[2, 2], sharex=ax2)
     plf.comp_speed_plot(data, subplot=True, ax=ax3)
 
     fig.suptitle(f'Sim Status: {analytics["status"]}\nLast Updated: {datetime.datetime.now().strftime("%d-%b-%Y %H:%M")}',
@@ -66,6 +73,34 @@ def sim_progress(output_loc, analytics):
     plt.tight_layout()
     plt.savefig(os.path.join(output_loc, 'time_progress.png'), bbox_inches="tight")
     plt.show()
+
+
+def cycle_plots(output_loc):
+    data = pd.read_csv(os.path.join(output_loc, 'data', 'cycle_info.csv'))
+
+    sns.set()
+    fig = plt.figure(figsize=(15,9))
+
+    widths = [1, 1]
+    heights = [1, 1]
+    spec = fig.add_gridspec(ncols=2, nrows=2, width_ratios=widths, height_ratios=heights)
+
+    ax1 = fig.add_subplot(spec[0, 0])
+    plf.cycle_stats_pm_plot(data, data_type='vel_err', subplot=True, ax=ax1)
+    ax2 = fig.add_subplot(spec[0, 1], sharex=ax1)
+    plf.cycle_stats_pm_plot(data, data_type='press_err', subplot=True, ax=ax2)
+    ax3 = fig.add_subplot(spec[1, 0], sharex=ax1)
+    plf.cycle_stats_plot(data, data_type='press_itr', subplot=True, ax=ax3)
+    ax4 = fig.add_subplot(spec[1, 1], sharex=ax1)
+    plf.hrr_plot(os.path.join(output_loc, 'data'), subplot=True, ax=ax4)
+
+    fig.suptitle(f'Last Updated: {datetime.datetime.now().strftime("%d-%b-%Y %H:%M")}',
+                     fontsize=12, va='top')
+
+    plt.savefig(os.path.join(output_loc, 'cycle_plots.png'), bbox_inches="tight")
+    plt.show()
+
+
 
 # PARAMETERS
 output_loc = r'C:\work\fds_tools\fds_diagnostics\tests\NTU_sc1_r3'
@@ -93,6 +128,7 @@ analytics = {'status': 'Running', 'pred': mock_pred}
 
 sim_progress(output_loc, analytics)
 mesh_plots(output_loc)
+cycle_plots(output_loc)
 
 # plf.timestep_bar_plot(data_loc, 'cpu_tot')
 # plf.hrr_plot(data_loc)
